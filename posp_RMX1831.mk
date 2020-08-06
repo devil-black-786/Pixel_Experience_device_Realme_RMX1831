0c1d281c369c1c352d6f3c5adc8ae4c94aa273fc
Imdeepak0001 <63868546+Imdeepak0001@users.noreply.github.com>
2020-08-06 14:10:08 +0530

    Update and rename aosp_RMX1831.mk to posp_RMX1831.mk

--- posp_RMX1831.mk
+++ posp_RMX1831.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/posp/config/common_full_phone.mk)
 
 # Inherit from realme RMX1831
 $(call inherit-product, device/realme/RMX1831/device.mk)
